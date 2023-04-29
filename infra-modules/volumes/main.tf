9eed5c8a252cc35544fe90a3965bf7a0ca4a7a6e
phandox <922425+phandox@users.noreply.github.com>
2023-04-29 10:36:13 +0200

    fix: comment out the initial filesystem on volumes - causes recreation
    reported to https://github.com/digitalocean/terraform-provider-digitalocean/issues/984

--- infra-modules/volumes/main.tf
+++ infra-modules/volumes/main.tf
@@ -2,8 +2,8 @@ resource "digitalocean_volume" "volume" {
   name                     = var.name
   region                   = var.region
   size                     = var.size
-  initial_filesystem_type  = "ext4"
-  initial_filesystem_label = var.fs_label
+#  initial_filesystem_type  = "ext4"
+#  initial_filesystem_label = var.fs_label
   tags                     = concat(var.tags, [var.env])
 }
 
